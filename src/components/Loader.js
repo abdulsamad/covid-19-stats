4cd62819572cd658a8ac1d6268d09627c90a4268
Abdul Samad <Samk8778@yahoo.com>
2020-04-06 16:52:13 +0530

    :lipstick: Added loader height

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import { Preloader } from 'react-materialize';
 
 function Loader() {
 	return (
-		<section className='section center-align'>
+		<section className='section center-align loader-container'>
 			<Preloader active color='red' flashing={false} size='big' />
 		</section>
 	);
