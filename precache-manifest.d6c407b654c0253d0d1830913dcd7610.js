dd1c4f128eb0127768a82f9b97b43f152f3ffb20
Abdul Samad <Samk8778@yahoo.com>
2020-09-27 21:14:22 +0530

    Updates

---
+++ precache-manifest.d6c407b654c0253d0d1830913dcd7610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f900a280613544c131e6f1cd06d5b6",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "7dd692703370923d2b02",
+    "url": "/covid-19/static/css/main.ea44b3a5.chunk.css"
+  },
+  {
+    "revision": "3c3bf59260c1a4169ccc",
+    "url": "/covid-19/static/js/2.ea046b78.chunk.js"
+  },
+  {
+    "revision": "81c55e1bb8a6bbefaf9572bfc44297fe",
+    "url": "/covid-19/static/js/2.ea046b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd692703370923d2b02",
+    "url": "/covid-19/static/js/main.3168b9e3.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  },
+  {
+    "revision": "ced5790ff9ef344da35bee0c1413c969",
+    "url": "/covid-19/static/media/indiaFlag.ced5790f.png"
+  }
+]);
