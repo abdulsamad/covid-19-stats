fac020384061c3fd08eb32f72cfb28e91f6e5afd
Abdul Samad <Samk8778@yahoo.com>
2021-04-08 16:36:25 +0530

    :art: Improved code formatting with prettier

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,143 +1,143 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Row, Col } from 'react-materialize';
-import Loader from './Loader';
+import React, { useState, useEffect } from "react";
+import { Card, Row, Col } from "react-materialize";
+import Loader from "./Loader";
 
 function Global({ country }) {
-	const [loading, setloading] = useState(true);
-	const [count, setCount] = useState({});
-	const [newCases, setNewCases] = useState({});
-	const [lastUpdated, setLastUpdated] = useState('');
-	const [countryCode, setCountryCode] = useState('');
+  const [loading, setloading] = useState(true);
+  const [count, setCount] = useState({});
+  const [newCases, setNewCases] = useState({});
+  const [lastUpdated, setLastUpdated] = useState("");
+  const [countryCode, setCountryCode] = useState("");
 
-	useEffect(() => {
-		if (country !== null) {
-			fetch(`https://api.covid19api.com/summary`)
-				.then((res) => res.json())
-				.then((res) => {
-					const countries = res.Countries;
+  useEffect(() => {
+    if (country !== null) {
+      fetch(`https://api.covid19api.com/summary`)
+        .then((res) => res.json())
+        .then((res) => {
+          const countries = res.Countries;
 
-					for (const key in countries) {
-						const current = countries[key];
-						if (current['Slug'] === country.slug) {
-							const totalCount = {
-								confirmed: current.TotalConfirmed,
-								recovered: current.TotalRecovered,
-								deaths: current.TotalDeaths,
-							};
+          for (const key in countries) {
+            const current = countries[key];
+            if (current["Slug"] === country.slug) {
+              const totalCount = {
+                confirmed: current.TotalConfirmed,
+                recovered: current.TotalRecovered,
+                deaths: current.TotalDeaths,
+              };
 
-							const newCount = {
-								confirmed: current.NewConfirmed,
-								recovered: current.NewRecovered,
-								deaths: current.NewDeaths,
-							};
+              const newCount = {
+                confirmed: current.NewConfirmed,
+                recovered: current.NewRecovered,
+                deaths: current.NewDeaths,
+              };
 
-							setCount(totalCount);
-							setNewCases(newCount);
-							setCountryCode(current.CountryCode);
-							break;
-						}
-					}
+              setCount(totalCount);
+              setNewCases(newCount);
+              setCountryCode(current.CountryCode);
+              break;
+            }
+          }
 
-					setLastUpdated(res.Date.slice(0, 10));
+          setLastUpdated(res.Date.slice(0, 10));
 
-					/* Loading */
-					setloading(false);
-				});
-		}
-	}, [country]);
+          /* Loading */
+          setloading(false);
+        });
+    }
+  }, [country]);
 
-	const formatNumber = (num) => {
-		if (num <= 0 || num === undefined) {
-			return '-';
-		} else {
-			return new Intl.NumberFormat('en-IN', {
-				maximumSignificantDigits: 8,
-			}).format(num);
-		}
-	};
+  const formatNumber = (num) => {
+    if (num <= 0 || num === undefined) {
+      return "-";
+    } else {
+      return new Intl.NumberFormat("en-IN", {
+        maximumSignificantDigits: 8,
+      }).format(num);
+    }
+  };
 
-	if (loading) {
-		return <Loader />;
-	}
+  if (loading) {
+    return <Loader />;
+  }
 
-	return (
-		<div>
-			<section className='center-align'>
-				<h4>{country.name}</h4>
-				<img
-					src={`https://www.countryflags.io/${countryCode}/flat/64.png`}
-					alt={`${country} Flag`}
-					height='64px'
-					width='64px'
-				/>
-				<br />
-			</section>
+  return (
+    <div>
+      <section className="center-align">
+        <h4>{country.name}</h4>
+        <img
+          src={`https://www.countryflags.io/${countryCode}/flat/64.png`}
+          alt={`${country} Flag`}
+          height="64px"
+          width="64px"
+        />
+        <br />
+      </section>
 
-			{/* Total Cases */}
-			<h5 className='center-align'>Count</h5>
-			<section className='section'>
-				<Card>
-					<Row className=''>
-						<Col s={6} className='total-cases'>
-							<h6 className='center-align'>Total</h6>
-							<h5 className='center-align'>{formatNumber(count.confirmed)}</h5>
-						</Col>
-						<Col s={6} className='total-recovered'>
-							<h6 className='center-align'>Recovered</h6>
-							<h5 className='center-align'>{formatNumber(count.recovered)}</h5>
-						</Col>
-					</Row>
-					<Row>
-						<Col s={6} className='total-deaths'>
-							<h6 className='center-align'>Deaths</h6>
-							<h5 className='center-align'>{formatNumber(count.deaths)}</h5>
-						</Col>
-						<Col s={6}>
-							<h6 className='center-align'>Last Updated</h6>
-							<h5 className='center-align'>{lastUpdated}</h5>
-						</Col>
-					</Row>
-					<div className='center-align'>
-						<blockquote>Source: covid19api.com</blockquote>
-					</div>
-				</Card>
-			</section>
+      {/* Total Cases */}
+      <h5 className="center-align">Count</h5>
+      <section className="section">
+        <Card>
+          <Row className="">
+            <Col s={6} className="total-cases">
+              <h6 className="center-align">Total</h6>
+              <h5 className="center-align">{formatNumber(count.confirmed)}</h5>
+            </Col>
+            <Col s={6} className="total-recovered">
+              <h6 className="center-align">Recovered</h6>
+              <h5 className="center-align">{formatNumber(count.recovered)}</h5>
+            </Col>
+          </Row>
+          <Row>
+            <Col s={6} className="total-deaths">
+              <h6 className="center-align">Deaths</h6>
+              <h5 className="center-align">{formatNumber(count.deaths)}</h5>
+            </Col>
+            <Col s={6}>
+              <h6 className="center-align">Last Updated</h6>
+              <h5 className="center-align">{lastUpdated}</h5>
+            </Col>
+          </Row>
+          <div className="center-align">
+            <blockquote>Source: covid19api.com</blockquote>
+          </div>
+        </Card>
+      </section>
 
-			{/* New Cases */}
-			<h5 className='center-align'>New Cases</h5>
-			<section className='section'>
-				<Card>
-					<Row>
-						<Col s={6} className='total-cases'>
-							<h5 className='center-align'>New Cases</h5>
-							<h5 className='center-align'>
-								{formatNumber(newCases.confirmed)}
-							</h5>
-						</Col>
-						<Col s={6} className='total-recovered'>
-							<h5 className='center-align'>New Recovered</h5>
-							<h5 className='center-align'>
-								{formatNumber(newCases.recovered)}
-							</h5>
-						</Col>
-					</Row>
-					<Row>
-						<Col s={6} className='total-deaths'>
-							<h5 className='center-align'>New Deaths</h5>
-							<h5 className='center-align'>{formatNumber(newCases.deaths)}</h5>
-						</Col>
-						<Col s={6}>
-							<h5 className='center-align'>Last Updated</h5>
-							<h5 className='center-align'>{lastUpdated}</h5>
-						</Col>
-					</Row>
-					<div className='center-align'>
-						<blockquote>Source: covid19api.com</blockquote>
-					</div>
-				</Card>
-			</section>
-		</div>
-	);
+      {/* New Cases */}
+      <h5 className="center-align">New Cases</h5>
+      <section className="section">
+        <Card>
+          <Row>
+            <Col s={6} className="total-cases">
+              <h5 className="center-align">New Cases</h5>
+              <h5 className="center-align">
+                {formatNumber(newCases.confirmed)}
+              </h5>
+            </Col>
+            <Col s={6} className="total-recovered">
+              <h5 className="center-align">New Recovered</h5>
+              <h5 className="center-align">
+                {formatNumber(newCases.recovered)}
+              </h5>
+            </Col>
+          </Row>
+          <Row>
+            <Col s={6} className="total-deaths">
+              <h5 className="center-align">New Deaths</h5>
+              <h5 className="center-align">{formatNumber(newCases.deaths)}</h5>
+            </Col>
+            <Col s={6}>
+              <h5 className="center-align">Last Updated</h5>
+              <h5 className="center-align">{lastUpdated}</h5>
+            </Col>
+          </Row>
+          <div className="center-align">
+            <blockquote>Source: covid19api.com</blockquote>
+          </div>
+        </Card>
+      </section>
+    </div>
+  );
 }
 
 export default Global;
