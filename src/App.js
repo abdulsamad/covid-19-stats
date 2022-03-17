e21ca5be45d35e163039ea02999a895f11da8be7
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-03-17 17:00:27 +0530

    Added materialize import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.scss';
 // eslint-disable-next-line
+import M from 'materialize-css';
 import Nav from './components/Nav';
 import Autocomplete from './components/Autocomplete';
 import Global from './components/Global';
