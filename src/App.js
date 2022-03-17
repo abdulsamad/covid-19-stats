e937ac42ae57d9b9221fe7f44edaee9b7019c40d
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-03-17 16:54:57 +0530

    Removed Covid19India API component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-import "./App.scss";
+import React, { useState } from 'react';
+import './App.scss';
 // eslint-disable-next-line
-import M from "materialize-css";
-import Nav from "./components/Nav";
-import India from "./components/India";
-import Autocomplete from "./components/Autocomplete";
-import Global from "./components/Global";
-import Footer from "./components/Footer";
+import Nav from './components/Nav';
+import Autocomplete from './components/Autocomplete';
+import Global from './components/Global';
+import Footer from './components/Footer';
 
 function App() {
   const [country, setCountry] = useState({
-    name: "India",
-    slug: "india",
+    name: 'India',
+    slug: 'india',
   });
 
   const updateSearched = (country) => {
@@ -23,7 +21,7 @@ function App() {
       <Nav title="Covid-19 Stats" />
       <Autocomplete updateSearched={updateSearched} />
       <div className="container">
-        {country.slug === "india" ? <India /> : <Global country={country} />}
+        <Global country={country} />
       </div>
       <Footer />
     </div>
