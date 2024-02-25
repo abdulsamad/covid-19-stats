cbe1b4aa9efab94123e0e7b2f914a8581951bcef
Abdul Samad Ansari <hello@abdulsamad.dev>
2024-02-25 14:39:29 +0530

    Updates

---
+++ precache-manifest.d5859a4f4b9581e9cc54222f24f5cbc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5e14a8605109cc0d0cf8cca1b6e891",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "c3a241d83d79b8bd207c",
+    "url": "/covid-19-stats/static/css/main.46471ce2.chunk.css"
+  },
+  {
+    "revision": "1d7b1a84600af39b4b70",
+    "url": "/covid-19-stats/static/js/2.46102457.chunk.js"
+  },
+  {
+    "revision": "81c55e1bb8a6bbefaf9572bfc44297fe",
+    "url": "/covid-19-stats/static/js/2.46102457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a241d83d79b8bd207c",
+    "url": "/covid-19-stats/static/js/main.3a7d577d.chunk.js"
+  },
+  {
+    "revision": "1ea630b52daa83a795cd",
+    "url": "/covid-19-stats/static/js/runtime-main.7cf40888.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0caee518020fe7497d5aca7b09289fd9.js"
+  "/covid-19-stats/precache-manifest.d5859a4f4b9581e9cc54222f24f5cbc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-stats/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
